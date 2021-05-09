9619065e92ae7d4d25b712629ef1d3dfdbfdeff9
TriggersWP <kaew.wanchalerm@gmail.com>
2021-05-09 16:58:20 +0700

    up

--- src/MyClass.java
+++ src/MyClass.java
@@ -4,7 +4,6 @@ public class MyClass {
             long a = 1234L;
             int b = 567;
             int c = (int)a * b;  //ปกติผลลัพธ์ต้องเป็นชนิด long แต่เราเปลี่ยนชนิด long เป็น int ให้ตรงกับชนิดผลัพธ์ (c)
-
             System.out.print(c);
       }
 }
