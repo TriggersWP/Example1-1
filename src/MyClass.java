f04069f1b80829210d357d752a058d329009ada9
Wanchalerm Panprasong <kaew.wanchalerm@gmail.com>
2021-05-09 15:12:44 +0700

    update

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+      public static void main(String[] args) {
+
+            long a = 1234L;
+            int b = 567;
+            int c = (int)a * b;  //ปกติผลลัพธ์ต้องเป็นชนิด long แต่เราเปลี่ยนชนิด long เป็น int ให้ตรงกับชนิดผลัพธ์ (c)
+
+            System.out.print(c);
+      }
+}
